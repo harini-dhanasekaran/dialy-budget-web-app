69aa8e72570c280b932560785a88e0de0b478f5e
Harini Dhanasekaran <63070913+harini-dhanasekaran@users.noreply.github.com>
2020-08-21 14:57:32 +0530

    first commit'

---
+++ app.js
@@ -0,0 +1,80 @@
+var budgetController =(function(){
+     var Income=function(id,description,value){
+        this.id=id;
+        this.description=description;
+        this.value=value;
+     };
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data={
+        allItems:{
+            exp: [],
+            inc: []
+        },
+        totals:{
+            exp:0,
+            inc:0
+        }
+    };
+
+
+})();
+
+var UIController = (function(){
+        var DOMstrings={
+            inputType: '.add__type',
+            inputDescription: '.add__description',
+            inputValue: '.add__value',
+            buttonAccess: '.add__btn'
+        }
+       return {
+           getInput :function(){
+               return{
+               type: document.querySelector(DOMstrings.inputType).value,
+               description : document.querySelector(DOMstrings.inputDescription).value,
+               value : document.querySelector(DOMstrings.inputValue).value
+
+           };
+        },
+            getDOMstrings :function(){
+                return DOMstrings;
+            }
+       };
+
+})();
+
+var controller = (function(budgetctrl,uictrl){
+
+    var setupEventListner=function(){
+        var DOM = uictrl.getDOMstrings();
+        //to get input when button is pressed
+        
+        document.querySelector(DOM.buttonAccess).addEventListener('click', ctrlAddItem);
+
+        // to get input when enter is pressed
+
+        document.addEventListener('keypress', function (event) {
+            if (event.keyCode === 13 || event.which === 13)
+                ctrlAddItem();
+
+         });
+    };
+    
+    var ctrlAddItem= function(){
+        var input=uictrl.getInput();
+        console.log(input);
+    };
+
+    return{
+        init: function(){
+            console.log("good");
+            setupEventListner();
+        }
+    };
+})(budgetController,UIController);
+
+controller.init();
